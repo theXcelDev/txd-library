336e43c132f0e0320654e1dff40a33af48da5029
theXcelDev <excellence.devsolutions@gmail.com>
2025-05-27 14:28:19 -0300

    chore: setting React Router up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { MainRouter } from './routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={MainRouter} />
   </StrictMode>
 );
---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+export const MainRouter = createBrowserRouter([
+  {
+    index: true,
+    element: <>Hello World!</>
+  }
+]);
